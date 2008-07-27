440a0d75bdb9885407686957c0559077b47f2a00
Brian Marshall <bmars@sdf.lonestar.org>
2008-07-27 09:10:12 -0700

    clear selection first last commit didn't do it all the time, apparently

--- content/css.js
+++ content/css.js
@@ -241,6 +241,7 @@ var GameFOXCSS =
     css[category][filename][map[column.index]] = value;
     prefs.setCharPref('theme.css.serialized', css.toSource());
 
+    this.selection.clearSelection();
     this.selection.select(idx);
 
     GameFOXCSS.reload();
