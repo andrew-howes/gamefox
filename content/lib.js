f3e809301ad60bc3a9524111822296f56af72dd5
Brian Marshall <bmars@sdf.lonestar.org>
2008-08-06 21:46:17 -0700

    Clean up lib.js Move all sidebar-related functions to sidebar.js, any other functions to GFlib

--- content/lib.js
+++ content/lib.js
@@ -59,47 +59,16 @@ var GFlib =
 
     if (doc.defaultView.parent != doc.defaultView.self) // we're in a frame
       doc.defaultView.parent.document.title = doc.title;
+  },
+
+  newTab: function(url, focus)
+  {
+    var browser = Components.classes['@mozilla.org/appshell/window-mediator;1'].
+      getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow(
+          'navigator:browser').getBrowser();
+
+    var tab = browser.addTab(url);
+    if (focusType == 0)
+      browser.selectedTab = tab;
   }
 };
-
-function gfox_addTab(aUrl, focusType)
-{
-  var browserWindow = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow('navigator:browser');
-  var browser       = browserWindow.getBrowser();
-  var newTab        = browser.addTab(aUrl);
-  if (focusType == 0) {
-    browser.selectedTab = newTab;
-  }
-}
-
-function gfox_processAboutDialog() {}
-
-function gfox_processSidebar()
-{
-  // For link middle-clicking
-  var links = document.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++)
-  {
-    links[i].setAttribute('onmousedown', 'if (event.button == 1) gfox_addTab(this.href, 1)');
-  }
-}
-
-function gfox_newTabLogin(loginForm)
-{
-  gfox_redirectLogin(loginForm);
-
-  var browserWindow = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow('navigator:browser');
-  browserWindow.BrowserOpenTab();
-
-  var oldSubmitEvent = loginForm.getAttribute('onsubmit');
-  loginForm.removeAttribute('onsubmit');
-  loginForm.submit();
-  loginForm.setAttribute('onsubmit', oldSubmitEvent);
-}
-
-function gfox_redirectLogin(loginForm)
-{
-  var doc    = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow('navigator:browser').content.document;
-  var path   = loginForm.ownerDocument.getElementById('gamefaqs-login-path');
-  path.value = (doc.location.protocol.match(/^https?:$/i) && doc.location.host.match(/(^|\.)gamefaqs\.com$/i)) ? doc.location.href.replace(/&(action)=[^&]*(?=&|$)|\b(action)=[^&]*&/ig, '') : 'http://www.gamefaqs.com/boards/';
-}
