9fbb15a91a0887ef965531422fbe63016f525adc
Michael Ryan <rockmfr@gmail.com>
2008-10-05 13:55:08 -0400

    Refactor sig.js
    * Since we're assuming that the default sig is first in both the menu and
      the serialized sig pref, there are quite a few simplifications that can
      be done. selectedIndex is now used instead of selectedItem.value in most
      places, unless there is an action occuring (i.e., "new"). If we were to
      ever list the sigs in a different order than how they are serialized, it
      would be pretty easy to go back to using selectedItem.value, and we would
      still have to re-write how indexing works anyways.
    * Shouldn't be any behavior changes in this commit.

--- content/sig.js
+++ content/sig.js
@@ -7,74 +7,81 @@ var GFsig =
 
   getSigByCriteria: function(account, boardname, boardid)
   {
+    account = account.toLowerCase();
+    boardname = boardname.toLowerCase();
+    var matches = new Array(new Array(), new Array(), new Array());
+    var accounts, boards;
     var sigs = eval(GFutils.getString('serialized', this.prefs));
-    if (account == null && boardname == null && boardid == null) // get default
-      return sigs[0];
-    else // get based on current account and board
+
+    // find matching sigs
+    for (var i = 1; i < sigs.length; i++)
     {
-      account = account.toLowerCase();
-      boardname = boardname.toLowerCase();
-      var matches = new Array(new Array(), new Array(), new Array());
-      var accounts, boards;
-      for (i in sigs)
+      // skip empty sigs
+      if (!sigs[i]['body'].length) continue;
+
+      accounts = GFutils.trim(sigs[i]['accounts'].toLowerCase()).split(/\s*;\s*/);
+      boards = GFutils.trim(sigs[i]['boards'].toLowerCase()).split(/\s*;\s*/);
+
+      // force the array length to 0
+      if (accounts.join() == '') accounts = new Array();
+      if (boards.join() == '') boards = new Array();
+
+      if (!accounts.length && boards.length)
       {
-        if (i == 0) continue; // skip default, it has lowest priority
-
-        // skip empty sigs
-        if (!sigs[i]['body'].length) continue;
-
-        accounts = GFutils.trim(sigs[i]['accounts'].toLowerCase()).split(/\s*;\s*/);
-        boards = GFutils.trim(sigs[i]['boards'].toLowerCase()).split(/\s*;\s*/);
-
-        // force the array length to 0
-        if (accounts.join() == '') accounts = new Array();
-        if (boards.join() == '') boards = new Array();
-
-        if (!accounts.length && boards.length && this.matchBoard(boards,
-              boardid, boardname))
+        if (this.matchBoard(boards, boardid, boardname))
           matches[1].push(sigs[i]);
-        else if (accounts.length && !boards.length && accounts.indexOf(account) != -1)
+      }
+      else if (accounts.length && !boards.length)
+      {
+        if (accounts.indexOf(account) != -1)
           matches[1].push(sigs[i]);
-        else if (accounts.length && boards.length && accounts.indexOf(account) != -1
-            && this.matchBoard(boards, boardid, boardname)) // account and board-specific sig, highest priority
-          matches[0].push(sigs[i]);
-        else if (!accounts.length && !boards.length) // global sig, lowest priority
-          matches[2].push(sigs[i]);
       }
-      // add the default if it's non-empty
-      if (sigs[0]['body'].length)
-        matches[2].push(sigs[0]);
-
-      var selectionPref = this.prefs.getIntPref('selection');
-      var sig;
-
-      if (selectionPref == 1 || selectionPref == 2)
+      else if (accounts.length && boards.length)
       {
-        // merge all the arrays
-        var allMatches = new Array();
-        for (i in matches)
-          for (j in matches[i])
-            allMatches.push(matches[i][j]);
-        if (selectionPref == 1)
-          sig = allMatches[0];
-        else // selectionPref == 2
-          sig = allMatches[Math.round(Math.random() * (allMatches.length - 1))];
+        if (accounts.indexOf(account) != -1
+            && this.matchBoard(boards, boardid, boardname))
+          matches[0].push(sigs[i]); // account and board-specific sig, highest priority
       }
-      else // selectionPref == 3
+      else
       {
-        var bestIndex;
-        if (matches[0].length)
-          bestIndex = 0;
-        else if (matches[1].length)
-          bestIndex = 1;
-        else
-          bestIndex = 2;
-        sig = matches[bestIndex][Math.round(Math.random() * (matches[bestIndex].length - 1))];
+        matches[2].push(sigs[i]); // global sig, lowest priority
       }
-      if (sig == undefined)
-        sig = sigs[0];
-      return sig;
     }
+    // default is the last of the lowest priority
+    if (sigs[0]['body'].length)
+      matches[2].push(sigs[0]);
+
+    var sig;
+    var selectionPref = this.prefs.getIntPref('selection');
+    if (selectionPref == 1 || selectionPref == 3)
+    {
+      // highest specificity
+      var bestIndex;
+      if (matches[0].length)
+        bestIndex = 0;
+      else if (matches[1].length)
+        bestIndex = 1;
+      else
+        bestIndex = 2;
+      if (selectionPref == 1)
+        sig = matches[bestIndex][0];
+      else // selectionPref == 3
+        sig = matches[bestIndex][Math.round(Math.random() * (matches[bestIndex].length - 1))];
+    }
+    else // selectionPref == 2
+    {
+      // no specificity
+      var allMatches = new Array();
+      for (i = 0; i < matches.length; i++)
+        for (var j = 0; j < matches[i].length; j++)
+          allMatches.push(matches[i][j]);
+      sig = allMatches[Math.round(Math.random() * (allMatches.length - 1))];
+    }
+    // default is only sig and is empty
+    if (sig == undefined)
+      sig = sigs[0];
+
+    return sig;
   },
 
   getSigById: function(id)
@@ -84,34 +91,27 @@ var GFsig =
 
   prepareOptionsPane: function()
   {
-    var sig = document.getElementById('sig-body');
     var menu = document.getElementById('sig-menu');
+    var sig = document.getElementById('sig-body');
 
-    // default sig must be global
+    // default sig is initially selected
     this.hideCriteriaForm();
-
-    // fill in fields
-    sig.value = this.getSigByCriteria()['body'];
+    sig.value = this.getSigById(0)['body'];
 
     // loop through sigs and add them to menulist
     var sigs = eval(GFutils.getString('serialized', this.prefs));
-    for (i in sigs)
+    for (var i = 1; i < sigs.length; i++)
     {
-      if (i == 0) continue;
       menu.insertItemAt(i, this.getCriteriaString(sigs[i]['accounts'],
-            sigs[i]['boards']) + ' / ' + sigs[i]['body'], i);
+            sigs[i]['boards']) + ' / ' + sigs[i]['body'], '');
     }
 
     this.updateCharCounts();
   },
 
+  // update forms
   menuCommand: function()
   {
-    // update forms based on the newly-selected signature
-    // default sig must not have the accounts and boards form
-    // new sig must create a new blank signature and select it
-    // any other signature must show the accounts and boards form
-    //
     var menu = document.getElementById('sig-menu');
     var accounts = document.getElementById('sig-criteria-accounts');
     var boards = document.getElementById('sig-criteria-boards');
@@ -119,30 +119,29 @@ var GFsig =
 
     if (menu.selectedItem.value == 'new')
     {
-      this.showCriteriaForm();
-      this.resetForm();
-      document.getElementById('sig-delete').disabled = true;
-
+      // create blank sig and select it
       var index = this.add();
-      menu.insertItemAt(index, 'Global signature', index);
+      menu.insertItemAt(index, 'Global signature', '');
       menu.selectedIndex = index;
     }
 
-    if (menu.selectedItem.value == 'default')
+    if (menu.selectedIndex == 0)
     {
+      // default sig has no accounts/boards and can't be deleted
       this.hideCriteriaForm();
       document.getElementById('sig-delete').disabled = true;
 
       accounts.value = '';
       boards.value = '';
-      sig.value = this.getSigByCriteria()['body'];
+      sig.value = this.getSigById(0)['body'];
     }
     else
     {
+      // other sigs have accounts/boards and can be deleted
       this.showCriteriaForm();
-      document.getElementById('sig-delete').disabled = false; // immutable
+      document.getElementById('sig-delete').disabled = false;
 
-      var sigData = this.getSigById(menu.selectedItem.value);
+      var sigData = this.getSigById(menu.selectedIndex);
       accounts.value = sigData['accounts'];
       boards.value = sigData['boards'];
       sig.value = sigData['body'];
@@ -165,8 +164,7 @@ var GFsig =
   {
     var menu = document.getElementById('sig-menu');
     var sigs = eval(GFutils.getString('serialized', this.prefs));
-    var idx = menu.selectedItem.value;
-    if (idx == 'default') idx = 0;
+    var idx = menu.selectedIndex;
 
     switch (event.id)
     {
@@ -174,18 +172,17 @@ var GFsig =
       case 'sig-criteria-boards': sigs[idx]['boards'] = event.value; break;
       case 'sig-body': sigs[idx]['body'] = event.value; break;
     }
-
     GFutils.setString('serialized', sigs.toSource(), this.prefs);
-    menu.selectedItem.label = this.getCriteriaString(sigs[idx]['accounts'],
-        sigs[idx]['boards'], idx == 0) + (idx != 0 ? ' / ' + sigs[idx]['body'] : '');
+
+    if (idx != 0) // don't set default
+      menu.selectedItem.label = this.getCriteriaString(sigs[idx]['accounts'],
+          sigs[idx]['boards']) + ' / ' + sigs[idx]['body'];
 
     this.updateCharCounts();
   },
 
-  getCriteriaString: function(accounts, boards, isDefault)
+  getCriteriaString: function(accounts, boards)
   {
-    if (isDefault) return 'Default signature';
-
     switch ((/\S/.test(accounts) ? 1 : 0) + (/\S/.test(boards) ? 2 : 0))
     {
       case 0: return 'Global signature';
@@ -207,36 +204,23 @@ var GFsig =
         '', null);
   },
 
-  resetForm: function()
-  {
-    document.getElementById('sig-criteria-accounts').value = '';
-    document.getElementById('sig-criteria-boards').value = '';
-    document.getElementById('sig-body').value = '';
-  },
-
   deleteSig: function()
   {
     var menu = document.getElementById('sig-menu');
-    var sigs = eval(GFutils.getString('serialized', this.prefs));
 
-    if (menu.selectedItem.value == 'default') return false;
+    // this should never happen
+    if (menu.selectedIndex == 0 || menu.selectedItem.value == 'new')
+      return;
 
     // remove it
-    sigs.splice(menu.selectedItem.value, 1);
+    var sigs = eval(GFutils.getString('serialized', this.prefs));
+    sigs.splice(menu.selectedIndex, 1);
     GFutils.setString('serialized', sigs.toSource(), this.prefs);
     menu.removeItemAt(menu.selectedIndex);
 
     // return to default signature
     menu.selectedIndex = 0;
     this.menuCommand();
-
-    // re-index menuitems
-    var j = 1;
-    var menuitems = menu.getElementsByTagName('menuitem');
-    for (i in menuitems)
-      if (menuitems[i] && menuitems[i].value != 'new' && menuitems[i].value != 'default'
-          && menuitems[i].value)
-        menuitems[i].value = j++;
   },
 
   matchBoard: function(boards, boardid, boardname)
