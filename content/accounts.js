58707e0cbbb7ea47efce0127786789c6a7d8c328
Brian Marshall <bmars@sdf.lonestar.org>
2008-10-17 22:41:48 -0700

    Add license blocks to new files
    Names are based on git's history of the files. Older files (those that
    were created before the git repo) weren't touched.

--- content/accounts.js
+++ content/accounts.js
@@ -1,4 +1,21 @@
-/* vim: set et sw=2 ts=2 sts=2 tw=79: */
+/* vim: set et sw=2 ts=2 sts=2 tw=79:
+ *
+ * Copyright 2008 Michael Ryan, Brian Marshall
+ *
+ * This file is part of GameFOX.
+ *
+ * GameFOX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * GameFOX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GameFOX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 var GFaccounts =
 {
