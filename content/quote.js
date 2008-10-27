f0c0314ddbf60ce98c155e688af0c00051b61735
Brian Marshall <bmars@sdf.lonestar.org>
2008-10-27 14:14:31 -0700

    Change quote header format
    From: user | Posted: date | num
    Let's see how this works out.

--- content/quote.js
+++ content/quote.js
@@ -128,8 +128,8 @@ var GFquote =
 
     /* Prepare quote header */
     var qhead = '';
-    if (GameFOX.prefs.getBoolPref('quote.header.username')) qhead += username;
-    if (GameFOX.prefs.getBoolPref('quote.header.date')) qhead += ' | Posted ' + postdate;
+    if (GameFOX.prefs.getBoolPref('quote.header.username')) qhead += "From: " + username;
+    if (GameFOX.prefs.getBoolPref('quote.header.date')) qhead += ' | Posted: ' + postdate;
     if (GameFOX.prefs.getBoolPref('quote.header.messagenum')) qhead += ' | ' + postnum;
 
     if (qhead.length && GameFOX.prefs.getCharPref('quote.style') == 'normal')
