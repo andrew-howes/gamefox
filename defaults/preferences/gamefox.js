90549f1bcbded6b27dc36638496e389a6c525e0f
Brian Marshall <bmars@sdf.lonestar.org>
2008-11-24 00:34:15 -0800

    Enable HTML buttons by default
    Seems like a good idea. The only thing I'm worried about is abuse of
    quote and code blocks with the buttons in easy reach of morons.
    Hopefully, the potential for abuse is minimal.

--- defaults/preferences/gamefox.js
+++ defaults/preferences/gamefox.js
@@ -1,6 +1,6 @@
 pref("gamefox.elements.quickpost.form", true);
 pref("gamefox.elements.quickpost.button", true);
-pref("gamefox.elements.quickpost.htmlbuttons", false);
+pref("gamefox.elements.quickpost.htmlbuttons", true);
 pref("gamefox.elements.quickpost.otherbuttons", true);
 pref("gamefox.elements.quickpost.link", true);
 pref("gamefox.elements.quickpost.link.title", "QuickPost");
