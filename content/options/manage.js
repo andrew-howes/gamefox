3c12168af23b2e2b1ddad3d7a9327faba1150cc1
Brian Marshall <bmars@sdf.lonestar.org>
2009-01-14 13:08:22 -0800

    Split manage pane functions into separate file

---
+++ content/options/manage.js
@@ -0,0 +1,150 @@
+/* vim: set et sw=2 ts=2 sts=2 tw=79:
+ *
+ * Copyright 2008, 2009 Brian Marshall, Michael Ryan
+ *
+ * This file is part of GameFOX.
+ *
+ * GameFOX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * GameFOX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GameFOX.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var GFmanageOptions =
+{
+  savePrefs: function()
+  {
+    var panes = document.getElementById('gamefox-prefwindow').preferencePanes;
+    for (var i = 0; i < panes.length; i++)
+      panes[i].writePreferences();
+  },
+
+  resetOptionsDialog: function(firstRun, notifications)
+  {
+    window.close();
+    GFlib.openOptionsDialog(firstRun, notifications, true);
+  },
+
+  importPrefs: function(button)
+  {
+    var manageMsg = document.getElementById('manageMsg');
+    button.setAttribute('disabled', true);
+
+    var strbundle = document.getElementById('manage-strings');
+
+    var filePicker = Cc['@mozilla.org/filepicker;1']
+      .createInstance(Ci.nsIFilePicker);
+    filePicker.init(window, strbundle.getString('importFilePicker'),
+        Ci.nsIFilePicker.modeOpen);
+    filePicker.appendFilters(filePicker.filterAll);
+    if (filePicker.show() != Ci.nsIFilePicker.returnOK)
+    {
+      button.setAttribute('disabled', false);
+      return;
+    }
+
+    var inputData = '';
+    var fiStream = Cc['@mozilla.org/network/file-input-stream;1']
+      .createInstance(Ci.nsIFileInputStream);
+    var siStream = Cc['@mozilla.org/scriptableinputstream;1']
+      .createInstance(Ci.nsIScriptableInputStream);
+    fiStream.init(filePicker.file, 0x01, 0, 0);
+    siStream.init(fiStream);
+    var str;
+    while ((str = siStream.read(4096)).length > 0)
+      inputData += str;
+    siStream.close();
+    fiStream.close();
+
+    try
+    {
+      var importedPrefs = eval(inputData);
+    }
+    catch (e)
+    {
+      GFutils.showNotification(manageMsg,
+          strbundle.getString('invalidSyntax'), 'warning');
+      button.setAttribute('disabled', false);
+      return;
+    }
+    GFprefs.clearUserPrefs();
+    for (var i in importedPrefs)
+      GFprefs.setPrefValue(i, importedPrefs[i]);
+
+    button.setAttribute('disabled', false);
+
+    this.resetOptionsDialog(false,
+        {'manageMsg':
+          [{label: strbundle.getString('importSuccess'),
+           type: 'info'}]
+        });
+  },
+
+  exportPrefs: function(button)
+  {
+    var manageMsg = document.getElementById('manageMsg');
+    button.setAttribute('disabled', true);
+
+    var strbundle = document.getElementById('manage-strings');
+
+    var filePicker = Cc['@mozilla.org/filepicker;1']
+      .createInstance(Ci.nsIFilePicker);
+    filePicker.init(window, strbundle.getString('exportFilePicker'),
+        Ci.nsIFilePicker.modeSave);
+    filePicker.appendFilters(filePicker.filterAll);
+    filePicker.defaultString = 'gamefox-prefs.txt';
+    var showValue = filePicker.show();
+    if (showValue != Ci.nsIFilePicker.returnOK
+        && showValue != Ci.nsIFilePicker.returnReplace)
+    {
+      button.setAttribute('disabled', false);
+      return;
+    }
+
+    this.savePrefs();
+    var outputData = GFprefs.getSerializedUserPrefs();
+    var foStream = Cc['@mozilla.org/network/file-output-stream;1']
+      .createInstance(Ci.nsIFileOutputStream);
+    foStream.init(filePicker.file, 0x02 | 0x08 | 0x20, 0664, 0);
+    foStream.write(outputData, outputData.length);
+    foStream.close();
+
+    GFutils.showNotification(manageMsg,
+        strbundle.getString('exportSuccess'), 'info');
+    button.setAttribute('disabled', false);
+  },
+
+  resetPrefs: function(button)
+  {
+    var manageMsg = document.getElementById('manageMsg');
+    button.setAttribute('disabled', true);
+
+    var strbundle = document.getElementById('manage-strings');
+
+    if (!GFlib.confirm(strbundle.getString('resetConfirm')))
+    {
+      button.setAttribute('disabled', false);
+      return;
+    }
+
+    GFprefs.clearUserPrefs();
+    GFlib.prefs.setCharPref('version', GFlib.version);
+
+    button.setAttribute('disabled', false);
+
+    GFcss.init();
+    GFcss.reload();
+    this.resetOptionsDialog(true,
+        {'manageMsg':
+          [{label: strbundle.getString('resetSuccess'),
+            type: 'warning'}]
+        });
+  }
+};
