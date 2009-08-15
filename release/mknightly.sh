488ff32c4524bbe1815f3f7cd2d3034addb81f42
Brian Marshall <bmars@sdf.lonestar.org>
2009-08-14 19:48:00 -0700

    Update mknightly.sh
    This is the first change for improving the release system. Moving the
    mccoy directory to mccoy.nightly lets me add another mccoy profile for
    stable versions (since they have different keys for no reason in
    particular other than it's not worth changing it now). I'll discuss the
    implications of this soon.
    
    I'm letting the next nightly run to make sure I haven't broken anything
    before continuing.
    
    * Remove bashisms, set interpreter to /bin/sh
    * Add variables for the needed directories

--- release/mknightly.sh
+++ release/mknightly.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 XPI=$1
 VERSION=$2
 BASEVERSION=$3
 
+SPOCK=spock
+MCCOY=mccoy.nightly
+
 echo
 
-if [[ ! -d spock ]]; then
-    echo "You need the spock binary in release/spock/" 1>&2
-elif [[ ! -d mccoy ]]; then
-    echo "You need the mccoy profile in release/mccoy/" 1>&2
+if [ ! -d $SPOCK ]; then
+    echo "You need the spock binary in `readlink -f $SPOCK`" 1>&2
+elif [ ! -d $MCCOY ]; then
+    echo "You need the mccoy profile in `readlink -f $MCCOY`" 1>&2
 else
-    ./spock/spock ./update.rdf -i urn:mozilla:extension:{6dd0bdba-0a02-429e-b595-87a7dfdca7a1} \
-        -d ./mccoy/ -v $VERSION -u http://beyondboredom.net/gfox/nightly/gamefox-$BASEVERSION.xpi \
-        -f ../$XPI > nightly.rdf || exit 1
+    $SPOCK/spock ./update.rdf \
+    -i urn:mozilla:extension:{6dd0bdba-0a02-429e-b595-87a7dfdca7a1} \
+    -d $MCCOY -v $VERSION \
+    -u http://beyondboredom.net/gfox/nightly/gamefox-$BASEVERSION.xpi \
+    -f ../$XPI > nightly.rdf || exit 1
 
     ./put.py ../$XPI nightly/gamefox-$BASEVERSION.xpi \
         nightly.rdf nightly/nightly.rdf || exit 1
