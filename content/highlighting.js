44d812f8375db30fdafa4216d2b7d2c6b2e929e9
Andrianto Effendy <andrianto.effendy@gmail.com>
2010-01-24 19:37:16 +0700

    News & name

--- content/highlighting.js
+++ content/highlighting.js
@@ -1,6 +1,6 @@
 /* vim: set et sw=2 ts=2 sts=2 tw=79:
  *
- * Copyright 2008, 2009 Brian Marshall, Michael Ryan
+ * Copyright 2008, 2009 Brian Marshall, Michael Ryan, Andrianto Effendy
  *
  * This file is part of GameFOX.
  *
