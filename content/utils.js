andrew-howes/gamefox

commit 79ebbec85946c81b594944468666783977262276

Fix QuickPost on Brilliant
Made getBoardId and getTopicId use parseBoardLink to prevent differences
in regexes like this happening again. That's kinda my fault for not
seeing the getXId functions when I wrote parseBoardLink.

content/utils.js
@@ -96,14 +96,14 @@ var gamefox_utils =
 
   getBoardId: function(str)
   {
-    var match = str.match(/^\/boards\/(\d+)/);
-    return match ? match[1] : 0;
+    var params = gamefox_utils.parseBoardLink(str);
+    return params ? params['board'] : 0;
   },
 
   getTopicId: function(str)
   {
-    var match = str.match(/^\/boards\/[^\/]+\/(\d+)/);
-    return match ? match[1] : 0;
+    var params = gamefox_utils.parseBoardLink(str);
+    return params ? params['topic'] : 0;
   },
 
   getMsgDataDisplay: function(doc)
@@ -425,12 +425,12 @@ var gamefox_utils =
 
   parseBoardLink: function(link)
   {
-    var params = link.match(/boards\/(-?[0-9]+)([^\/]+)?\/([0-9]+)/);
+    var params = link.match(/boards\/(-?\d+)([^\/]+)?(\/\d+)?/);
 
     if (!params) // regex didn't match
       return false;
-
-    return { 'board': params[1], 'name': params[2].substr(1), 'topic': params[3] };
+    return { 'board': params[1], 'name': params[2].substr(1),
+      'topic': params[3].substr(1) };
   },
 
   // Strips admin and mod labels from usernames

commit 70c4956561c98c44405fa7b898f6566dadffeff1

Fix topic QuickPost
Caused by 79ebbec, this prevents an error when the topic id is undefined.

content/utils.js
@@ -430,7 +430,7 @@ var gamefox_utils =
     if (!params) // regex didn't match
       return false;
     return { 'board': params[1], 'name': params[2].substr(1),
-      'topic': params[3].substr(1) };
+      'topic': (params[3] ? params[3].substr(1) : null) };
   },
 
   // Strips admin and mod labels from usernames