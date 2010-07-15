1ed8c14def522268f7fc26ef672c64d3782d21a1
Brian Marshall <bmars@sdf.lonestar.org>
2010-07-15 15:57:00 -0700

    Update components for Fx4
    I switched to using XPCOMUtils, which is not an Fx4 thing, but greatly
    simplifies everything.
    
    Fx4 requires the components to be specified in chrome.manifest. It also
    requires profile-after-change to be used instead of app-startup.
    
    References:
    https://developer.mozilla.org/en/How_to_Build_an_XPCOM_Component_in_Javascript#Using_XPCOMUtils
    https://developer.mozilla.org/en/XPCOM/XPCOM_changes_in_Gecko_2.0

--- components/policy.js
+++ components/policy.js
@@ -1,30 +1,27 @@
 /* vim: set et sw=2 ts=2 sts=2 tw=79: */
 
-/***********************************************************
-constants
-***********************************************************/
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+
 const nsIContentPolicy = Components.interfaces.nsIContentPolicy;
 const nsISupports = Components.interfaces.nsISupports;
-const CLASS_ID = Components.ID('{2572941e-68cb-41a8-be97-cbb40611dcbc}');
-const CLASS_NAME = 'GameFOX content policy';
-const CONTRACT_ID = '@gamefox/contentpolicy;1';
 const adTest = /(2mdn\.net|adlegend\.com|advertising\.com|atdmt\.com|adimg\.cnet\.com|mads\.cnet\.com|surveys\.cnet\.com|adlog\.com\.com|dw\.com\.com|i\.i\.com\.com|contextweb\.com|doubleclick\.net|eyewonder\.com|adimg\.gamefaqs\.com|bwp\.gamefaqs\.com|mads\.gamefaqs\.com|bwp\.gamespot\.com|insightexpressai\.com|mediaplex\.com|pointroll\.com|questionmarket\.com|revsci\.net|scorecardresearch\.com|serving-sys\.com|specificclick\.net|tribalfusion\.com|turn\.com|unicast\.com|voicefive\.com|adserver\.yahoo\.com|yieldmanager\.com)$/;
 const host = 'www.gamefaqs.com';
 const prefs = Components.classes['@mozilla.org/preferences-service;1']
   .getService(Components.interfaces.nsIPrefService)
   .getBranch('gamefox.');
 
-/***********************************************************
-class definition
-***********************************************************/
-function GFcontentPolicy()
-{
-  this.wrappedJSObject = this;
-}
+function GFcontentPolicy() { }
 
 GFcontentPolicy.prototype =
 {
-  shouldLoad: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra)
+  classDescription: 'GameFOX content policy',
+  classID: Components.ID('{2572941e-68cb-41a8-be97-cbb40611dcbc}'),
+  contractID: '@gamefox/contentpolicy;1',
+  _xpcom_categories: [{ category: 'content-policy' }],
+  QueryInterface: XPCOMUtils.generateQI([nsIContentPolicy, nsISupports]),
+
+  shouldLoad: function(contentType, contentLocation, requestOrigin, context,
+      mimeTypeGuess, extra)
   {
     try
     {
@@ -49,82 +46,15 @@ GFcontentPolicy.prototype =
     }
   },
 
-  shouldProcess: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra)
+  shouldProcess: function(contentType, contentLocation, requestOrigin, context,
+      mimeTypeGuess, extra)
   {
     return nsIContentPolicy.ACCEPT;
   },
-
-  QueryInterface: function(aIID)
-  {
-    if (!aIID.equals(nsIContentPolicy) &&
-        !aIID.equals(nsISupports))
-      throw Components.results.NS_ERROR_NO_INTERFACE;
-    return this;
-  }
 };
 
-/***********************************************************
-class factory
-***********************************************************/
-var GFcontentPolicyFactory =
-{
-  createInstance: function(aOuter, aIID)
-  {
-    if (aOuter != null)
-      throw Components.results.NS_ERROR_NO_AGGREGATION;
-    return new GFcontentPolicy().QueryInterface(aIID);
-  }
-};
-
-/***********************************************************
-module definition (xpcom registration)
-***********************************************************/
-var GFcontentPolicyModule =
-{
-  registerSelf: function(aCompMgr, aLocation, aLoaderStr, aType)
-  {
-    aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar)
-      .registerFactoryLocation(CLASS_ID, CLASS_NAME, CONTRACT_ID,
-          aLocation, aLoaderStr, aType);
-
-    var catMgr = Components.classes['@mozilla.org/categorymanager;1']
-      .getService(Components.interfaces.nsICategoryManager);
-    catMgr.deleteCategoryEntry('content-policy', CONTRACT_ID, true);
-    catMgr.addCategoryEntry('content-policy', CONTRACT_ID, CONTRACT_ID, true,
-        true);
-  },
-
-  unregisterSelf: function(aCompMgr, aLocation, aLoaderStr)
-  {
-    aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar)
-      .unregisterFactoryLocation(CLASS_ID, aLocation);
-
-    Components.classes['@mozilla.org/categorymanager;1']
-      .getService(Components.interfaces.nsICategoryManager)
-      .deleteCategoryEntry('content-policy', CONTRACT_ID, true);
-  },
-
-  getClassObject: function(aCompMgr, aClass, aIID)
-  {
-    if (!aIID.equals(Components.interfaces.nsIFactory))
-      throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-
-    if (aClass.equals(CLASS_ID))
-      return GFcontentPolicyFactory;
-
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-  },
-
-  canUnload: function(aCompMgr)
-  {
-    return true;
-  }
-};
-
-/***********************************************************
-module initialization
-***********************************************************/
-function NSGetModule(aCompMgr, aFileSpec)
-{
-  return GFcontentPolicyModule;
-}
+// Backwards compatibility with Fx3
+if (XPCOMUtils.generateNSGetFactory)
+  var NSGetFactory = XPCOMUtils.generateNSGetFactory([GFcontentPolicy]);
+else
+  var NSGetModule = XPCOMUtils.generateNSGetModule([GFcontentPolicy]);
