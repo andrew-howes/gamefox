0513861739f3b6eeb78860a424c45d71f5a0056d
Brian Marshall <bmars@sdf.lonestar.org>
2011-10-29 07:06:31 -0700

    accounts.js copyright update

--- content/accounts.js
+++ content/accounts.js
@@ -1,6 +1,6 @@
 /* vim: set et sw=2 ts=2 sts=2 tw=79:
  *
- * Copyright 2008, 2009 Michael Ryan, Brian Marshall
+ * Copyright 2008, 2009, 2011 Michael Ryan, Brian Marshall
  *
  * This file is part of GameFOX.
  *
