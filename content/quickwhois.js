0ed88e3294e909d81eeb11b8ae0f70ab1d484f90
Brian Marshall <bmars@sdf.lonestar.org>
2011-11-07 01:15:28 -0800

    Make QuickWhois prettier (and refactor it)
    * I'm giving QuickWhois the same dropshadow treatment as the character map.
      Also, it has a cool opacity fade in/out.
    * It uses a table now for a cleaner layout
    * Email and websites get linkified by us
    * Code has been simplified and refactored. I removed all of the field name
      translations (e.g., "E-Mail" -> "Email") because it's kind of pointless to
      make it different from the actual profile page

--- content/quickwhois.js
+++ content/quickwhois.js
@@ -1,6 +1,6 @@
 /* vim: set et sw=2 ts=2 sts=2 tw=79:
  *
- * Copyright 2008, 2009 Brian Marshall, Michael Ryan
+ * Copyright 2008, 2009, 2011 Brian Marshall, Michael Ryan
  *
  * This file is part of GameFOX.
  *
@@ -21,99 +21,125 @@ var gamefox_quickwhois =
 {
   quickWhois: function(event)
   {
-    var node = event.target;
-    var doc = node.ownerDocument;
+    var doc = gamefox_lib.getDocument(event);
+    var pos = gamefox_quickwhois.getPos(event);
+    var row = gamefox_utils.findParent('td', event.target);
+    var qw = doc.evaluate('div[@class="gamefox-quickwhois"]', row, null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-    try
+    if (qw)
     {
-      while (node.nodeName.toLowerCase() != 'td')
-        node = node.parentNode;
-    }
-    catch (e) { return; }
+      if (qw.style.display == 'none')
+      {
+        qw.style.display = 'block';
+        qw.style.top = pos[0] + 'px';
+        qw.style.left = pos[1] + 'px';
+        window.setTimeout(function() { qw.style.opacity = '1'; }, 10);
+      }
+      else
+        qw.style.opacity = '0';
 
-    var div = doc.evaluate('div[@class="gamefox-quickwhois"]',
-        node, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    // offsets are for stylesheets that put a border on <html>
-    if (div)
-    {
-      div.style.display = div.style.display == 'none' ? '' : 'none';
-      div.style.top = window.content.scrollY + event.clientY + doc.body.parentNode.offsetTop + 'px';
-      div.style.left = window.content.scrollX + event.clientX + doc.body.parentNode.offsetLeft + 'px';
       return;
     }
 
-    div = doc.createElement('div');
-    div.className = 'gamefox-quickwhois';
-    div.style.top = window.content.scrollY + event.clientY + doc.body.parentNode.offsetTop + 'px';
-    div.style.left = window.content.scrollX + event.clientX + doc.body.parentNode.offsetLeft + 'px';
-    div.innerHTML = 'Loading QuickWhois...';
-    node.appendChild(div);
+    qw = doc.createElement('div');
+    qw.className = 'gamefox-quickwhois';
+    qw.style.top = pos[0] + 'px';
+    qw.style.left = pos[1] + 'px';
+    qw.textContent = 'Loading QuickWhois...';
+    qw.addEventListener('transitionend', function() {
+      if (qw.style.opacity == '0') qw.style.display = 'none'; }, false);
+    row.appendChild(qw);
+    window.setTimeout(function() { qw.style.opacity = '1'; }, 10);
 
     var request = new XMLHttpRequest();
-    request.open('GET', node.querySelector('a.name').href);
+    request.open('GET', row.querySelector('a.name').href);
     var ds = gamefox_lib.thirdPartyCookieFix(request);
     request.onreadystatechange = function()
     {
-      if (request.readyState == 4)
+      if (request.readyState != 4) return;
+
+      qw.textContent = '';
+      var table = doc.createElement('table');
+
+      var text = request.responseText;
+      var profileFieldsHTML = '';
+      var fields = [
+        'User ID',
+        'Board User Level',
+        'Account Created',
+        'Last Visit',
+        'E-Mail',
+        'Web Site',
+        'AIM (Username)',
+        'AIM (E-Mail)',
+        'Yahoo IM',
+        'Windows Live (MSN)',
+        'Google Talk',
+        'ICQ',
+        'Xbox Live',
+        'PlayStation Network',
+        'DS Friend Code',
+        'Wii Number',
+        'Wii Friend Code',
+        'Skype',
+        'Steam',
+        'xfire',
+        'Twitter',
+        'Signature',
+        'Quote',
+        'Karma',
+        'Contributor Page',
+        'My Games Page'
+      ];
+      var field, tr, td;
+
+      for (var i = 0; i < fields.length; i++)
       {
-        var profileFieldsHTML = '';
-        var profileFields = [
-            'User ID', 'User ID',
-            'Board User Level', 'User Level',
-            'Account Created', 'Created At',
-            'Last Visit', 'Last Visit',
-            'E-Mail', 'Email',
-            'Web Site', 'Website',
-            'AIM \\(Username\\)', 'AIM Username',
-            'AIM \\(E-Mail\\)', 'AIM Email',
-            'Yahoo IM', 'Yahoo IM',
-            'Windows Live \\(MSN\\)', 'MSN',
-            'Google Talk', 'Google Talk',
-            'ICQ', 'ICQ',
-            'Xbox Live', 'Xbox Live',
-            'PlayStation Network', 'PlayStation Network',
-            'DS Friend Code', 'DS Friend Code',
-            'Wii Number', 'Wii Number',
-            'Wii Friend Code', 'Wii Friend Code',
-            'Skype', 'Skype',
-            'Steam', 'Steam',
-            'xfire', 'Xfire',
-            'Twitter', 'Twitter',
-            'Signature', 'Signature',
-            'Quote', 'Quote',
-            'Karma', 'Karma'
-        ];
-        for (var i = 0; i < profileFields.length; i += 2)
+        if (field = gamefox_quickwhois.findInfo(fields[i], text))
         {
-          if ((profileField = gamefox_quickwhois.findInfo(profileFields[i], request.responseText))
-              != '')
-          {
-            if (profileFields[i] == 'Board User Level')
-              profileField = profileField.split(/<br\s*\/?>/i)[0].replace(
-                  /<\/?b>/gi, '');
-            profileFieldsHTML += '<b>' + profileFields[i+1] + ':</b> ' +
-              profileField.replace(/<br\s*\/?>/gi, '<br/>') + '<br/>';
-          }
+          tr = doc.createElement('tr');
+
+          td = doc.createElement('td');
+          td.textContent = fields[i];
+          tr.appendChild(td);
+
+          td = doc.createElement('td');
+          if (fields[i] == 'E-Mail')
+            td.innerHTML = '<a href="mailto:' + gamefox_utils.URLEncode(field)
+              + '">' + field + '</a>';
+          else if (fields[i] == 'Web Site')
+            td.innerHTML = '<a href="' + gamefox_utils
+              .specialCharsEncode(field) + '">' + field + '</a>';
+          else
+            td.innerHTML = field;
+          tr.appendChild(td);
+
+          table.appendChild(tr);
         }
-        div.innerHTML = profileFieldsHTML.replace(/<br\/>$/, '')
-          + gamefox_quickwhois.findInfo('Contributor Page', request.responseText).
-          replace(/^</, '<br/><')
-          + gamefox_quickwhois.findInfo('My Games', request.responseText).
-          replace(/^</, '<br/><');
       }
+
+      qw.appendChild(table);
     };
     request.send(null);
   },
 
   findInfo: function(what, where)
   {
-    var pattern = new RegExp('<th\\b[^>]*>(?:\\s*<a\\b[^>]*>)?\\s*' + what + '(?:\\s*</a>)?\\s*</th><td>([^\\0]*?)</td>', 'gi');
-    var matches = pattern.exec(where);
+    return (new RegExp('<th\\b[^>]*>(?:\\s*<a\\b[^>]*>)?\\s*' +
+          gamefox_utils.specialRegexpCharsEscape(what) +
+          '(?:\\s*</a>)?\\s*</th><td>([^\\0]*?)</td>', 'gi').exec(where) ||
+        [, ''])[1].trim();
+  },
 
-    if (matches)
-      return matches[1].trim();
+  getPos: function(event)
+  {
+    var doc = gamefox_lib.getDocument(event);
 
-    return '';
+    // offsets are for stylesheets that put a border on <html>
+    return [
+      window.content.scrollY + event.clientY + doc.body.parentNode.offsetTop,
+      window.content.scrollX + event.clientX + doc.body.parentNode.offsetLeft
+    ];
   }
 };
