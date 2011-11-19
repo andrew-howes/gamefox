2bbd6afd51009b5446224b4feacd3234c5b959a7
Brian Marshall <bmars@sdf.lonestar.org>
2011-11-18 19:11:01 -0800

    Fix topic/message list detection with ad in header
    This was causing message lists to be detected as topic lists, because the user
    panel contains the topic search form on both pages.

--- content/lib.js
+++ content/lib.js
@@ -208,7 +208,14 @@ var gamefox_lib =
           return true;
         }
 
-        if (doc.getElementById('searchtopicstring') ||
+        // Check if there is a topic search form, but make sure there's no
+        // "Topic List" link (the user panel will contain a search form even on
+        // message lists)
+        if ((doc.getElementById('searchtopicstring') &&
+              !doc.evaluate('.//div[@class="user_panel"]/div' +
+                '/div[@class="links"]/ul/li/a[contains(translate(., "TL", ' +
+                    '"tl"), "topic list")]', contentDiv, null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) ||
             contentDiv.getElementsByClassName('topics')[0])
         {
           doc.gamefox.pageType = ['topics'];
